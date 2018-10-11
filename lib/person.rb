0c17d7e09335e316fffb088a5b2af42e36ef4e02
github username <github email address>
2018-10-11 00:04:53 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,105 +1,106 @@
-require 'pry'
+require "pry"
+
 class Person
- attr_reader :name
-attr_accessor :bank_account, :happiness, :hygiene, :friend
 
+ attr_reader :name, :happiness, :hygiene
+ attr_accessor :bank_account
+
+ #function to initialize with name, bank balance
  def initialize(name)
-
-   @name=name
-   @bank_account=25
-   @happiness=8
-   @hygiene=8
-   @friend
+   @name = name
+   @bank_account = 25
+   @happiness = 8
+   @hygiene = 8
  end
 
- def name
-   @name
- end
+     def happiness=(happy_num)
+       @happiness = happy_num
+         if happy_num > 10
+         self.happiness = 10
 
- def happiness
-   @happiness
- end
-
- def bank_account
-   @bank_account
- end
- def hygiene
-   @hygiene
-
- end
- def friend
-   @friend
- end
+         elsif
+           happy_num < 0
+           self.happiness = 0
+         end
+     end
 
 
- def happiness=(new_happiness)
-   @happiness=new_happiness
-   if @happiness>=10
-   return @happiness = 10
- elsif @happiness<=0
-   return @happiness = 0
-   end
- end
+     def hygiene=(hygiene_num)
+       @hygiene = hygiene_num
+       if hygiene_num > 10
+         self.hygiene = 10
+       elsif
+         hygiene_num < 0
+         self.hygiene = 0
+       end
 
- def hygiene=(new_hygiene)
-   @hygiene=new_hygiene
-   if @hygiene>=10
-   return @hygiene= 10
- elsif @hygiene<=0
-   return @hygiene = 0
-   end
+     end
 
- def happy?
-   if @happiness>7
-     return true
-   else return false
-   end
- end
-end
-def clean?
- if @hygiene>7
-   return true
- else return false
- end
-end
-def get_paid(salary)
- @bank_account += salary
- return "all about the benjamins"
-end
+     def happy?
+       if self.happiness < 8
+         return false
+       else
+         return true
+       end
+     end
+
+     def clean?
+       if self.hygiene >= 8
+         return true
+       else
+         return false
+       end
+     end
+
+     def get_paid(salary)
+       self.bank_account = self.bank_account + salary
+       return 'all about the benjamins'
+     end
+
+     def take_bath
+       self.hygiene = @hygiene += 4
+       return "♪ Rub-a-dub just relaxing in the tub ♫"
+     end
+
+     def work_out
+       self.hygiene = @hygiene -= 3
+       self.happiness = @happiness += 2
+       return "♪ another one bites the dust ♫"
+     end
+
+     def call_friend(friend)
+       self.happiness += 3
+
+       friend.happiness += 3
 
 
-def take_bath
-self.hygiene += 4
-puts self.hygiene
-   return "♪ Rub-a-dub just relaxing in the tub ♫"
-end
+       return "Hi #{friend.name}! It's #{self.name}. How are you?"
 
-def work_out
-  self.happiness +=2
-  self.hygiene -=3
-  return "♪ another one bites the dust ♫"
-end
+     end
 
-def call_friend(friend)
+     def start_conversation(friend,topic)
+         if topic == "politics"
+           friend.happiness -= 2
+           self.happiness -= 2
+            return 'blah blah partisan blah lobbyist'
+          elsif
+            topic == "weather"
+             friend.happiness += 1
+             self.happiness += 1
+            return  'blah blah sun blah rain'
+          else
+            return "blah blah blah blah blah"
 
-  @friend = friend
-self.happiness += 3
-@friend.happiness +=3
+          end
+     end
 
-return "Hi #{friend.name}! It's #{name}. How are you?"
 
-end
-def start_conversation(person, topic)
- if topic == "politics"
-   self.happiness-= 2
- person.happiness-=2
-   'blah blah partisan blah lobbyist'
- elsif topic == "weather"
-   self.happiness += 1
-   person.happiness+=1
-   'blah blah sun blah rain'
- else 'blah blah blah blah blah'
- end
-end
+ # def bank_account
+ #
+ # end
 
+
+
+binding.pry
+puts "hi"
 end
